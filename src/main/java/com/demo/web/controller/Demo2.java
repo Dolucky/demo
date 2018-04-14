097c11414263598c032b56020867a8627c9af81c
lgdong <lovekbai999@vip.qq.com>
2018-04-14 16:57:38 +0800

    dev open

--- src/main/java/com/demo/web/controller/Demo2.java
+++ src/main/java/com/demo/web/controller/Demo2.java
@@ -3,9 +3,9 @@ package com.demo.web.controller;
 /**
  * Created by LGD on 2018/4/14.
  */
-public class Demo {
+public class Demo2 {
+
     public static void main(String[] args) {
-        System.out.println("dev");
-        System.out.println("....");
+
     }
 }
