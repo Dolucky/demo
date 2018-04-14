c4465041daafb1dd96a9369e962eb1cf280871ad
lgdong <lovekbai999@vip.qq.com>
2018-04-14 14:41:39 +0800

    dev 222

--- src/main/java/com/demo/web/controller/HelloController.java
+++ src/main/java/com/demo/web/controller/HelloController.java
@@ -6,6 +6,6 @@ package com.demo.web.controller;
 public class HelloController {
 
     public static void main(String[] args) {
-        System.out.println("我是dev 分支 .. ");
+        System.out.println("我是dev 分支2222 .. ");
     }
 }
